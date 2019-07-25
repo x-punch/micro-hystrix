3bddd72ca7c6012fa5638ce219cec4e6d1c8e1c3
johnson <chengqiaosheng@gmail.com>
2019-07-25 18:14:55 +0800

    add default configure values

--- configure.go
+++ configure.go
@@ -4,6 +4,19 @@ import (
 	"github.com/afex/hystrix-go/hystrix"
 )
 
+var (
+	// DefaultTimeout is how long to wait for command to complete, in milliseconds
+	DefaultTimeout = 1000
+	// DefaultMaxConcurrent is how many commands of the same type can run at the same time
+	DefaultMaxConcurrent = 10
+	// DefaultVolumeThreshold is the minimum number of requests needed before a circuit can be tripped due to health
+	DefaultVolumeThreshold = 20
+	// DefaultSleepWindow is how long, in milliseconds, to wait after a circuit opens before testing for recovery
+	DefaultSleepWindow = 5000
+	// DefaultErrorPercentThreshold causes circuits to open once the rolling measure of errors exceeds this percent of requests
+	DefaultErrorPercentThreshold = 50
+)
+
 // CommandConfig is used to tune circuit settings at runtime
 type CommandConfig struct {
 	Timeout                int
